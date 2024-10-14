cb4d4b8a7ef5cfdc3abbab4c23754f74499db63e
joana.casallas <joacasallas@gmail.com>
2024-10-14 18:05:46 -0500

    fixing tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import testBaseModel
 from models.state import State
 
 
-class test_state(test_basemodel):
+class test_state(testBaseModel):
     """ """
 
     def __init__(self, *args, **kwargs):
