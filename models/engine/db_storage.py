72183d73c66c67a792f7ad2af19902f89933e7a8
joana.casallas <joacasallas@gmail.com>
2024-11-30 07:19:49 -0500

    6. DBStorage - connect engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
+# Author: Joana Casallas
 """This module creates a connection with a relational database"""
 import os
-import MySQLdb
+from sqlalchemy import create_engine
+from models.base_model import Base
 
+class DBStorage:
+    """Establish a connection to the MySQL database"""
+    __engine = None
+    __session = None
+    def __init__(self):
+        """Creates the engine and session for database interaction"""
+        # Retrieve environment variables
+        env = os.environ.get('HBNB_ENV', 'dev')
+        host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
+        user = os.environ.get('HBNB_MYSQL_USER')
+        password = os.environ.get('HBNB_MYSQL_PWD')
+        database = os.environ.get('HBNB_MYSQL_DB')
 
-def get_db_connection():
-    """ Establish a connection to the MySQL database """
-    env = os.environ.get('HBNB_ENV', 'dev')
-    host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
-    user = os.environ.get('HBNB_MYSQL_USER')
-    password = os.environ.get('HBNB_MYSQL_PWD')
-    database = os.environ.get('HBNB_MYSQL_DB')
+        if not all([user, host, password, database]):
+            raise ValueError("Missing environment variables for DB configuration")
 
-    if not all([host, user, password, database]):
-        raise ValueError("Some required environment variables are missing")
-
-    if env == 'test':
-        database = f"{database}_test"
-
-    try:
-        db = MySQLdb.connect(
-            host=host,
-            user=user,
-            passwd=password,
-            db=database,
-            port=3306
-        )
-        print("Database connection established successfully.")
-        return db
-    except MySQLdb.OperationalError as e:
-        print(f"Error connecting to MySQL: {e}")
-        return None
-
-
-if __name__ == "__main__":
-    connection = get_db_connection()
-    if connection:
-        connection.close()
+        self.__engine = create_engine(
+            f'mysql+mysqldb://{user}:{password}@{host}/{database}',
+            pool_pre_ping=True
+            )
+        if env == 'test':
+            try:
+                connection = self.__engine.connect()
+                connection.execute("SET FOREIGN_KEY_CHECKS = 0")
+                result = connection.execute(
+                    "SELECT table_name FROM information_schema.tables WHERE table_schema = DATABASE();"
+                    )
+                for row in result:
+                    table_name = row[0]
+                    connection.execute(f"DROP TABLE IF EXISTS `{table_name}`;")
+            except Exception as e:
+                print(f"Error while dropping tables; {e}")
+            finally:
+                connection.close()
+        Base.metadata.create_all(self.__engine)
