c8f3d5f8e8eef4e2a2642ea91c33ac652b3b3577
joana.casallas <joacasallas@gmail.com>
2024-12-01 11:30:33 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,8 @@ class BaseModel:
         """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             for key, value in kwargs.items():
                 if key in ['updated_at', 'created_at']:
