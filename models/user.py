5270cf338508a5793539dc2d8da97f38460665f4
joana.casallas <joacasallas@gmail.com>
2024-12-02 18:29:18 -0500

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" City Module for HBNB project """
+import os
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    """Class User mapped to the users table"""
+    if os.getenv('STORAGE_TYPE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
