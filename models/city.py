ee13411bb0a55390ff5428ff1f63351a2cedff05
joana.casallas <joacasallas@gmail.com>
2024-12-04 15:37:02 -0500

    update relationship cities and places

--- models/city.py
+++ models/city.py
@@ -9,10 +9,7 @@ from models.base_model import BaseModel, Base
 class City(BaseModel, Base):
     """ Class City mapped to the cities table """
     __tablename__ = 'cities'
-    if os.getenv('STORAGE_TYPE') == 'db' or os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete", passive_deletes=True)
-    else:
-        name =""
-        state_id=""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete", passive_deletes=True)
+    
