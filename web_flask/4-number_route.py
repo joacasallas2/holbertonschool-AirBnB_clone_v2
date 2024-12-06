dd71f24cdaf2ce8dc2bcfad26144464807316e71
joana.casallas <joacasallas@gmail.com>
2024-12-06 10:10:14 -0500

    4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """display hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """display c text"""
+    sentence = f"c {escape(text)}"
+    sentence = sentence.replace("_", " ")
+    return sentence
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """Display Python text"""
+    sentence = f"Python {escape(text)}"
+    sentence = sentence.replace("_", " ")
+    return sentence
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """display n is a number only if n is integer"""
+    return f"{escape(n)} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
