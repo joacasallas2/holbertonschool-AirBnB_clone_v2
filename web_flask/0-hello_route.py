f90cc415e02fa3877fad0124ac050d1c1686ca1b
joana.casallas <joacasallas@gmail.com>
2024-12-06 07:23:12 -0500

    strict slashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def Hello():
     """ function that display Hello HBNB"""
     return "<p>Hello HBNB!</p>"
