b9643111a97c269bf42376860fc8ae116ffe1867
joana.casallas <joacasallas@gmail.com>
2024-12-08 10:18:22 -0500

    8. List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""Flask App"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list_function():
+    """Display a HTML page with the list of states"""
+    dict_objs = storage.all(State)
+    states_list = []
+    for obj in dict_objs.values():
+        state_id = obj.id
+        state_name = obj.name
+        states_list.append((state_id, state_name))
+    states_list.sort(key = lambda x: x[1])
+    return render_template('7-states_list.html', states_list=states_list)
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    """Close the storage session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
