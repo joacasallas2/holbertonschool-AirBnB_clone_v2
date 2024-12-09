413fb7ef414f04cac5f4314f1dc85a2799360a8a
joana.casallas <joacasallas@gmail.com>
2024-12-08 22:24:03 -0500

    jupdate item 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
 # Author: Joana Casallas
 """Flask Web Application to display a list of states """
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list_function():
-    """Display a HTML page with the list of states"""
-    dict_objs = storage.all(State)
-    states_list = sorted(list(dict_objs.values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states_list=states_list)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_db_connection(exception):
-    """Remove the current SQLAlchemy Session after each request."""
-    try:
-        storage.close()
-    except Exception as e:
-        print(f"Error closing session:. {e}")
-
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port='5000')
