c9e19f3e64ce67ae7bc00b3c5ca50cdb9181ca75
joana.casallas <joacasallas@gmail.com>
2024-12-08 22:20:50 -0500

    jupdate item 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
 @app.route("/states_list", strict_slashes=False)
 def states_list_function():
     """Display a HTML page with the list of states"""
