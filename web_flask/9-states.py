75da39b609e5e0f2ed27aecc813b91d43725f51e
joana.casallas <joacasallas@gmail.com>
2024-12-15 13:02:24 -0500

    10. States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,6 +2,7 @@
 # Author: Joana Casallas
 """Flask Web Application to display a list of states and its cities """
 from flask import Flask, render_template
+from markupsafe import escape
 from models import storage
 from models.state import State
 
@@ -9,14 +10,18 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states", defaults={'state_id': None}, strict_slashes=False)
-@app.route("/states/<state_id>", strict_slashes=False)
-def display_states(state_id):
+@app.route("/states", defaults={'id_number': None}, strict_slashes=False)
+@app.route("/states/<id_number>", strict_slashes=False)
+def display_states(id_number):
     """ list of all State objects present in DBStorage"""
+    state_id = f"{escape(id_number)}"
+    state_id = state_id.strip()
     dict_states = storage.all(State).values()
     dict_states = sorted(dict_states, key=lambda x: x.name)
     return render_template('9-states.html',
-                           dict_states=dict_states, state_id=state_id)
+                           dict_states=dict_states,
+                           state_id=state_id,
+                           id_number=id_number)
 
 
 @app.teardown_appcontext
