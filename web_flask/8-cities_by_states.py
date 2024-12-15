3b36bd3955f3934ba3e4713fc4b102d79a63ae43
joana.casallas <joacasallas@gmail.com>
2024-12-15 16:46:40 -0500

    trim_blocks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,6 +7,8 @@ from models.state import State
 
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.route("/cities_by_states", strict_slashes=False)
