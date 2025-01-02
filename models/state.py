3717ab8663c03c3ca6cf56ed5a35cf90e93c0f47
joana.casallas <joacasallas@gmail.com>
2025-01-02 15:05:08 -0500

    pep8 state

--- models/state.py
+++ models/state.py
@@ -11,7 +11,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+        cities = relationship('City',
+                              backref='state', cascade='all, delete-orphan')
     else:
         @property
         def cities(self):
