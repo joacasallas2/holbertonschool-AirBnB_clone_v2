7ef5240176fe0a1a07118efe1a356eb63d57010d
joana.casallas <joacasallas@gmail.com>
2025-01-02 14:44:31 -0500

    update state class handle dbstorage / filestorage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id"""
+            from models import storage
+            from models.city import City
+            all_cities_storage = storage.all(City)
+            list_cities_with_same_state_id = []
+            for city in all_cities_storage:
+                if city.state_id == self.id:
+                    list_cities_with_same_state_id.append(city)
+            return list_cities_with_same_state_id
