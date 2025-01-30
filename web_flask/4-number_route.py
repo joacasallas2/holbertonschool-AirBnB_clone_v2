35028ac7e4e7e76c8c8e956016117fa3cf1f5ecc
joana.casallas <joacasallas@gmail.com>
2025-01-30 16:05:42 -0500

    4. Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -36,5 +36,12 @@ def text_python(text="is cool"):
     return f"Python {escape(text)}"
 
 
+@app.route("/number/<int:n>", strict_slashes=False)
+def if_number(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return f"{escape(n)} is a number"
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
