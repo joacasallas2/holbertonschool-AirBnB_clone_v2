5322e7f32b14cd06d3f7a08603573993c0d22b40
joana.casallas <joacasallas@gmail.com>
2025-01-30 14:56:17 -0500

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,7 +12,7 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route("/HBNB", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Display HBNB"""
     return "HBNB"
