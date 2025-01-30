6662122ecaec8644892358570782aad5851d4edf
joana.casallas <joacasallas@gmail.com>
2025-01-30 14:46:25 -0500

    web flask - 0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""This module starts a Flask web application:"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """return hello"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
